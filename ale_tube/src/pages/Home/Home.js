18e29e4688ca75c0f8389d80f8835e4cea67832a
Gmoges <156920398+Gmoges@users.noreply.github.com>
2024-12-06 20:48:00 +0300

    second_step/image&menu

--- ale_tube/src/pages/Home/Home.js
+++ ale_tube/src/pages/Home/Home.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React from 'react';
+import './home.css'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className='first'>
+        
+        <div className='head_title'>
+             <h1>AleTube</h1>
+             <div className='menu'>
+                Home
+             </div>
+        </div>
+
+        <div className='menu_list'>
+                <li>HOME</li>
+                <li>CONTACT</li>
+                <li>YOUTUBE</li>
+                <li>INSTAGRAM</li>
+                <li>TIK-TOK</li>
+             </div>
+
+        <div className='icons'></div>
+    </section>
+    
   )
 }
 
