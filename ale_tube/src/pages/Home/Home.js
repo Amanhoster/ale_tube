e3824865a7d9f69129dd62c72af73ad0132fc437
Gmoges <156920398+Gmoges@users.noreply.github.com>
2024-12-13 22:04:10 +0300

    eddited_new

--- ale_tube/src/pages/Home/Home.js
+++ ale_tube/src/pages/Home/Home.js
@@ -3,26 +3,23 @@ import './home.css'
 
 const Home = () => {
   return (
-    <section className='first'>
-        
-        <div className='head_title'>
-             <h1>AleTube</h1>
-             <div className='menu'>
-                Home
-             </div>
-        </div>
 
-        <div className='menu_list'>
-                <li>HOME</li>
-                <li>CONTACT</li>
-                <li>YOUTUBE</li>
-                <li>INSTAGRAM</li>
-                <li>TIK-TOK</li>
-             </div>
 
-        <div className='icons'></div>
-    </section>
+
     
+    <section className='header'>
+      <div className='head_top'>
+        <h1>ALETUBE</h1>
+      </div>
+      <div className='head_top2'>
+        <ul className='ul1'>
+          <li className='cheak'>Home</li>
+          <li>Contact</li>
+          <li>About-us</li>
+          <li>Social media links</li>
+        </ul>
+      </div>
+    </section>
   )
 }
 
