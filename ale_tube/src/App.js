4a3030813bccc8544e16b329fb1656b3dcafbe2a
Gmoges <156920398+Gmoges@users.noreply.github.com>
2024-12-15 16:22:39 +0300

    all header(finshed)

--- ale_tube/src/App.js
+++ ale_tube/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Home from './pages/Home/Home';
+import Midle from './pages/Home_header2/Midle';
 
 function App() {
   return (
     <div className="App">
          <Home />
+         <Midle />
     </div>
   );
 }
---
+++ ale_tube/src/pages/Home_header2/Midle.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./midle.css"
+const Midle = () => {
+  return (
+    <div>Midle</div>
+  )
+}
+
+export default Midle
